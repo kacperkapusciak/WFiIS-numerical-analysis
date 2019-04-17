25fa70ff2b62642779802771182ca84463d9ddbd
kacperkapusciak <kapusciak.k@gmail.com>
2019-04-17 20:43:04 +0200

    lab7

---
+++ lab7/Main.cpp
@@ -0,0 +1,71 @@
+#include "math.h"
+#include <fstream>
+#include <iostream>
+
+const int N = 6;
+
+double funkcja(double x) {	
+	return (1.0 / (1.0 + pow(x, 2)));
+}
+
+double iloraz_roznicowy(double wezly[N+1], double wartosci_funkcji[N+1], int rzad_ilorazu) {
+    double wynik;
+    double mianownik;
+    for (int k = 0; k <= rzad_ilorazu; k++) {
+        mianownik = 1.0;
+        for (int l = 0; l <= rzad_ilorazu; l++) 
+            if (l != k) 
+                mianownik *= wezly[k] - wezly[l];
+            
+        wynik += wartosci_funkcji[k] / mianownik;
+    }
+    return wynik;
+}
+
+double wielomian_interpolacyjny(double wezly[N+1], double wartosci_funkcji[N+1], double x, double iloraz_roznicowy_tab[N+1]) {
+    double wynik;
+    double suma = 0.0;
+    for (int j = 0; j <= N; j++) {
+        wynik = 1.0;
+        for (int i = 0; i <= j-1; i++) 
+           wynik *= x - wezly[i]; 
+
+        suma += iloraz_roznicowy_tab[j] * wynik;
+   } 
+   return suma;
+}
+
+int main() {
+    // double wezly[N+1] = {-5, -2, -0.5, 0, 0.5, 2, 5};
+    double wezly[N+1];
+    double wartosci_funkcji[N+1];
+    double iloraz_roznicowy_tab[N+1];
+    double krok = 0.01;
+    std::ofstream file;
+    file.open("out.dat");
+
+    double step = (10.0 / (double)(N - 1));
+    for (int i = 1;  i < N +1; i++) 
+        wezly[i] = -5 + step * i;
+
+    wezly[0] = -5;
+    wezly[7] = 5;
+
+    for (int i = 0; i < N+1; i++) {
+        wartosci_funkcji[i] = funkcja(wezly[i]);
+        std::cout << wartosci_funkcji[i] << std::endl;
+    }
+
+    std::cout << "Ilorazy roznicowe: " << std::endl;
+    for (int i = 0; i < N+1; i++) {
+        iloraz_roznicowy_tab[i] = iloraz_roznicowy(wezly, wartosci_funkcji, i);
+        std::cout << iloraz_roznicowy(wezly, wartosci_funkcji, i) << std::endl;
+    }
+
+    for (double x = -5; x < 5; x += 0.01 ) {
+        file << x << "\t" << wielomian_interpolacyjny(wezly, wartosci_funkcji, x, iloraz_roznicowy_tab) << std::endl;
+    }
+
+    file.close();
+    return 0;
+}
